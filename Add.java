f36d4cdf08bafae65f0f6f9a1094fc336dae3d71
ankushk1928 <kadamankush1928@gmail.com>
2023-06-07 16:13:42 +0530

    Simple calcultor

---
+++ Add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Add {
+
+        public static void main(String[] args) {
+            int a;
+            int b;
+            int c;
+
+            Scanner in = new Scanner(System.in);
+
+            a = in.nextInt();
+            b = in.nextInt();
+            c = a + b;
+
+            System.out.println(c);
+
+        }
+}
---
+++ Multi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multi {
+    public static void main(String[] args){
+        int a;
+        int b;
+        int c;
+
+        Scanner in = new Scanner(System.in);
+
+        a = in.nextInt();
+        b = in.nextInt();
+        c = a * b;
+
+        System.out.println(c);
+
+    }
+}
